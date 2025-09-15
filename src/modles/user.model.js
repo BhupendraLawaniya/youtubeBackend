99386b12dcab0c4e7d3e60ceb8dc348624009c5c
bhupendralawaniya <bhupendra22081999@gmail.com>
2025-09-15 08:33:22 +0530

    connect with cloudinary

--- src/modles/user.model.js
+++ src/modles/user.model.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+const mongoose = require ('mongoose');
+const bcrypt = require ("bcrypt");
 const userSchema = new mongoose.Schema(
     {
         watchHistory:{
@@ -35,5 +36,40 @@ const userSchema = new mongoose.Schema(
         }
     },{timestamps:true}
 )
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username: this.userName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+};
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign({
+        _id:this._id,
+       
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+};
+
 
 export const User = mongoose.model('User', userSchema);
--- src/modles/video.model.js
+++ src/modles/video.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require ('mongoose');
 const videoSchema = new mongoose.Schema(
     {
         videoFile:{
