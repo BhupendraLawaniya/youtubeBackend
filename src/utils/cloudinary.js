753a39a302af02131d3d524bd2e3a1638c6ad939
bhupendralawaniya <bhupendra22081999@gmail.com>
2025-09-15 23:33:50 +0530

    install multer

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,30 @@
-const { v2: cloudinary } = require ('cloudinary');
+const { v2: cloudinary } = require('cloudinary');
 const fs = require('fs');
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = (localFilePath) => {
+  try {
+    if (!localFilePath) return null
+    const response = cloudinary.v2.uploader
+      .upload(localFilePath, {
+        resource_type: "auto",
+        public_id: "my_dog",
+        overwrite: true
+      })
+      .then(result => console.log(result));
+      console.log("File is uploaded on Cloudinary",response.url)
+      return response;
+
+  }
+  catch (error) {
+    fs.unlinkSync(localFilePath) //remove the locally saved file when upload operation is failed on cloudinary
+    return null;
+  }
+}
+module.exports = uploadOnCloudinary;
+
